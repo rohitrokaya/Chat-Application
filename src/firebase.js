0f55a76e2e08cca086806c1a17a1603304919907
Rohit Rokaya <102810403+rohitrokaya@users.noreply.github.com>
2023-10-23 03:50:52 +0545

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyCr89tryLCDib-KOC4e6aleWni8iBdxfTo",
-    authDomain: "chat-application-8ff7d.firebaseapp.com",
-    projectId: "chat-application-8ff7d",
-    storageBucket: "chat-application-8ff7d.appspot.com",
-    messagingSenderId: "1000522788422",
-    appId: "1:1000522788422:web:6db8ac69aaf60d5439e447",
-    measurementId: "G-P0CQLB2403"
+    apiKey: "AIzaSyBpRuWlmUcaWw16ZDAoYMSRHQikcd6MPGg",
+    authDomain: "chatapp-roshreta.firebaseapp.com",
+    databaseURL: "https://chatapp-roshreta-default-rtdb.firebaseio.com",
+    projectId: "chatapp-roshreta",
+    storageBucket: "chatapp-roshreta.appspot.com",
+    messagingSenderId: "318377310568",
+    appId: "1:318377310568:web:f66a4d9c9f774f63a83edd",
+    measurementId: "G-R4T1RFHW02"
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
